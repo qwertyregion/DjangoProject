8456c77db59682f5db3d563f372e75ab1435d887
QWERTY <qwerty05@mail.ru>
2023-07-29 03:19:11 +0300

    первый коммит

--- siteproject/main/views.py
+++ siteproject/main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseNotFound, Http404
 
 
 def home(request):
@@ -14,3 +15,14 @@ def next1(request):
     return render(request, template_name='next1.html', context=dict_name)
 
 
+def next2(request, year=None):
+    if year is None:
+        year = 'год не определен '
+        return render(request, template_name='next2.html', context={'year': year})
+    if int(year) < 2000 or int(year) > 2023:
+        return redirect('home', permanent=True)
+    return render(request, template_name='next2.html', context={'year': year})
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('ЭТА ССЫЛКА НЕДЕЙСТВИТЕЛЬНА', status=404)
